c9756b9397dd2ca8fdfb79b7a83601944a5d58c0
Bhavana Peri <64651468+BhavanaPeri@users.noreply.github.com>
2020-08-15 17:10:18 +0530

    Update Launcher.js

--- Launcher.js
+++ Launcher.js
@@ -6,22 +6,22 @@ class Launcher{
             stiffness: 0.04,
             length: 20
         }
-        this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
-    fly(){
-        this.sling.bodyA = null;
-    }
+    
     display(){
        // push();
         if(this.sling.bodyA){
         pointA=this.sling.bodyA.position;
         pointB = this.pointB;
         stroke(0);
-        strokeWeight(5);
+        strokeWeight(2);
         line(pointA.x,pointA.y,pointB.x,pointB.y)
         //pop();
         }
+    }
+    fly(){
+        this.sling.bodyA = null;
     }
 }
